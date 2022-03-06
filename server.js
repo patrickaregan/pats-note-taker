2637bfbb00f998e049b96e72815ab03413199ad4
patrickaregan <patrickaregan@gmail.com>
2022-03-06 00:26:34 -0600

    implement base MVP functionality

---
+++ server.js
@@ -0,0 +1,54 @@
+// ****************************************
+// Require resources
+// ****************************************
+const express = require('express');
+const fs = require('fs');
+const notes = require('./db/db');
+const path = require('path');
+
+// ****************************************
+// Functions
+// ****************************************
+const createNewNote = (body) => {
+    
+    return notes;
+}
+
+
+// ****************************************
+// Configure App
+// ****************************************
+const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ****************************************
+// Create Routes
+// ****************************************
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length;
+    const newNote = req.body;
+    notes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes), null, 2);
+    res.json(newNote);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+// ****************************************
+// Start App
+// ****************************************
+app.listen(3001, () => {
+    console.log('App server now listening on port 3001!');
+})
